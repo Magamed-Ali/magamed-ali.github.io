58e49e87df22dbc59c8942c4ae6a08dc5b0fd262
gazmagamaev <magamedali93@mail.ru>
2021-04-01 22:58:33 +0300

    js/progeckt/3

---
+++ javascript-3/jscript.js
@@ -0,0 +1,50 @@
+// 1) Задание
+console.log("1) Задание")
+function number_a(a){
+    if (a<10){
+        return "константа меньше 10"
+    }
+    else{
+        return "константа не меньше 10"
+    }
+}
+console.log(number_a(15))
+
+// 2) Задание
+console.log("2) Задание")
+function _max(a, b, c){
+    if (a>b){
+        return a
+    }
+    else if (b>c){
+        return b
+    }
+    else{
+        return c
+    }
+}
+console.log("максимальное число " + _max(1, 15, 10)  )
+
+
+// 3) Задание
+console.log("3) Задание")
+function getDiscountSum(x, y){
+    return x * (y/100)
+}
+console.log("скидка - " + getDiscountSum(15000, 20))
+
+// 4) Задание
+function F(a, b){
+    if (a.length < b){
+        return "Ошибка, слишком маленький текст"
+    }
+    else{
+        return a.substr(-b)
+    }
+}
+console.log(F("dfdfsdfdf", 10))
+
+
+//5 Задание
+console.log("5) Задание")
+console.log("Выводимая переменная в consol является глобальной, функция test() не объявлена, ответ 120.")
