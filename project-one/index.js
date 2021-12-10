aac0d34c282dde195cb668ee01852f0ca8170297
gazmagamaev <magamedali93@mail.ru>
2021-12-10 21:40:32 +0300

    new-item

--- project-one/index.js
+++ project-one/index.js
@@ -9,4 +9,35 @@ const {coordinates:
 { start: [startX, startY],
   end: [endX, endY]}} = shape;
 
-  console.log(startX, startY, endX, endY);
+
+var a = {
+  name: "it-kamasutra.com",
+  protocol: "https",
+  maxStudentsCount: 10,
+  isOnline: true,
+  student: ['ivan', 'vdfd','asdf'],
+  classroom: {
+    teatcher: {
+      name: 'wew',
+      age: 18
+    }
+  }
+}
+
+console.log(a)
+let b = {...a};
+
+b.isOnline = false;
+
+console.log(b)
+
+a.name = "sadfasdf";
+
+console.log(a)
+
+b.name = "adsfsdd";
+b.isOnline = null;
+a.isOnline = false
+a.name = "aa"
+console.log(b)
+console.log(a)
